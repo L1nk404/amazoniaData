551897ae7aae20d55cb40ad6baa0ce0da72bb9a6
link <gabrielhmorais98@gmail.com>
2025-04-22 13:05:49 -0300

    criação arquivos data

---
+++ src/util/FileAccess.java
@@ -0,0 +1,14 @@
+package util;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class FileAccess 
+{
+        // public static List<Data> loadData(String filePath)
+        // {
+        //     List<Data> dataList = new ArrayList<>();
+
+        //     return dataList;
+        // }
+}
