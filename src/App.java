42625c0bd5075dc34059a8a6ebb72d7d64624c71
link <gabrielhmorais98@gmail.com>
2025-04-25 23:39:36 -0300

    loadData class pronto

--- src/App.java
+++ src/App.java
@@ -11,9 +11,24 @@ public class App
         String FilePath = (OS == "Windows") ?
                             "..\\data\\data.csv" : "../data/data.csv";
 
-        // teste
-        Utils.clearScreen();
+        // // teste
+        // Utils.clearScreen();
 
-        System.out.printf("%s\n%s\n", OS,FilePath);
+        // System.out.printf("%s\n%s\n", OS,FilePath);
+
+
+        String[] array = {"123", "salada", "1.27", "-1", "batata", "-13.9898"}; // Java infers the size (4)
+
+        for (String num : array)
+        {
+            if (num.matches("-?\\d+.?\\d+"))
+            {
+                System.out.printf("%s: digit\n", num);
+            }
+            else
+            {
+                System.out.printf("%s: is String\n", num);
+            }
+        }
     }
 }
